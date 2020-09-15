32b67228c50b7c7b96099d5db45e23f92320f25d
Michael <you@example.com>
2020-09-15 21:21:48 +0200

    Add a test case with conflicting access from three threads

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 SOURCES := test.cpp
 OBJS := $(SOURCES:.cpp=.o)
+LDFLAGS=-pthread
+
 
 test: $(OBJS)
-	$(CXX) $(CXXFLAGS) -o test $(OBJS) $(LFLAGS) $(LIBS)
+	$(CXX) $(CXXFLAGS) -o test $(OBJS) $(LDFLAGS) $(LIBS)
 
 .cpp.o:
 	$(CXX) $(CXXFLAGS) $(INCLUDES) -c $<
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 
 #define CATCH_CONFIG_MAIN
+#include <thread>
 
 #include "Catch2/single_include/catch2/catch.hpp"
 #include "locked.hpp"
@@ -10,3 +11,30 @@ TEST_CASE("Inner int can be incremented", "int") {
   REQUIRE(i.get() == 6);
 }
 
+TEST_CASE("Int incrementing race", "int") {
+  const constexpr int count = 1000000;
+  locked<int> l1(0);
+  locked<int> l2(0);
+  locked<int> l3(0);
+
+  auto incrementer = [&l1, &l2, &l3, &count]() {
+    static const auto increment = [](int& value) { value++; };
+    for (int i = 0; i < count; i++) {
+      l1.mut(increment);
+      l2.mut(increment);
+      l3.mut(increment);
+    }
+  };
+
+  auto thread1 = std::thread(incrementer);
+  auto thread2 = std::thread(incrementer);
+  auto thread3 = std::thread(incrementer);
+
+  thread1.join();
+  thread2.join();
+  thread3.join();
+
+  REQUIRE(3 * count == l1.get());
+  REQUIRE(3 * count == l2.get());
+  REQUIRE(3 * count == l3.get());
+}
